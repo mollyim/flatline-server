62619055b4e96bf3922c7388da20f6e66e50927f
Uoemai <218074189+uoemai@users.noreply.github.com>
2025-07-04 12:44:35 +0200

    build: build containers in Maven with Jib

--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker compose up -d --remove-orphans
-docker logs -f flatline-server
--- docker/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker compose -f compose.test.yaml up --build --remove-orphans --abort-on-container-exit --exit-code-from server 
