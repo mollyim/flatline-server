d33c7869783603d9726776127c951ecb6ec2386d
Uoemai <218074189+uoemai@users.noreply.github.com>
2025-07-21 12:21:28 +0200

    dev: fix some DynamoDB tables with CloudFormation

--- dev/localstack/whisper-service.sh
+++ dev/localstack/whisper-service.sh
@@ -5,3 +5,7 @@ awslocal s3 mb s3://whisper-service-dynamic-config
 awslocal s3 cp /tmp/dev.yml s3://whisper-service-dynamic-config/dev.yml
 
 awslocal s3 mb s3://whisper-service-pre-key-store
+
+awslocal cloudformation deploy \
+  --stack-name whisper-service-dynamodb \
+  --template-file /etc/localstack/init/ready.d/whisper-service-dynamodb.yaml
